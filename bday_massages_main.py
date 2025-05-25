39f5e022903960df2147ab981e7e9d21b2725623
sinthia <sinegghoqrb@gmail.com>
2025-05-25 23:57:55 +0100

    Slot Machine

---
+++ bday_massages_main.py
@@ -0,0 +1,16 @@
+# import datetime, bday_messages
+#
+# from bday_messages import random_message
+#
+# today = datetime.date.today()
+# next_birthday =  datetime.date(2025, 11, 27)
+# days_away = next_birthday - today
+#
+# print(days_away)
+#
+# if today == next_birthday:
+#     print(random_message)
+# else:
+#     print(f"My next birthday is {days_away} days away!")
+
+
